34133809ccf51a829312bce4c925d2696e4fe5e5
Sylvia <sylviakieha@gmail.com>
2015-09-21 00:33:08 +0300

    added to products listing

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,13 @@ module ProductsHelper
   def print_price(price)
   number_to_currency price
   end
+
+  def print_stock(stock)
+    if stock > 0
+      content_tag(:span, "In Stock (##)", class: "in_stock")
+    else
+      content_tag(:span, "Out of Stock", class: "out_stock")
+    end
+  end
+
 end
