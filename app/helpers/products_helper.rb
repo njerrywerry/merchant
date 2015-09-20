409b12a26bd17a28b2443137525e64db906fed02
Sylvia <sylviakieha@gmail.com>
2015-09-21 00:57:34 +0300

    dealing with stock and its display

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,7 +5,7 @@ module ProductsHelper
 
   def print_stock(stock)
     if stock > 0
-      content_tag(:span, "In Stock (##)", class: "in_stock")
+      content_tag(:span, "In Stock", class: "in_stock")
     else
       content_tag(:span, "Out of Stock", class: "out_stock")
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ActiveRecord::Base
   input.delete!("$")
   super
   end
+
+  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0}
+
 end
