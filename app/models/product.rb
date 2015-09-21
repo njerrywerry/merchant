daabd1ebac741db2d66e2a557009880f899dccc7
Sylvia <sylviakieha@gmail.com>
2015-09-21 13:33:18 +0300

    related orders, order items and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0}
+  has_many :order_items
+
   def price=(input)
   input.delete!("$")
   super
   end
-
-  validates :stock, numericality: {only_integer:true, greater_than_or_equal_to:0}
 
 end
