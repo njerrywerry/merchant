b15768bac82e2fa4c44c7818a9795282d1cb13fe
Sylvia <sylviakieha@gmail.com>
2015-09-23 10:47:48 +0300

    created carts for order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,5 @@
 class OrderItemsController < ApplicationController
+  before_action :load_order, only: [:create]
   before_action :set_order_item, only: [:show, :edit, :update, :destroy]
 
   # GET /order_items
@@ -24,11 +25,11 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    @order_item = OrderItem.new(order_item_params)
+    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
 
     respond_to do |format|
       if @order_item.save
-        format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
+        format.html { redirect_to @order, notice: 'Successfully added product to cart.' }
         format.json { render :show, status: :created, location: @order_item }
       else
         format.html { render :new }
@@ -63,6 +64,17 @@ class OrderItemsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+  def load_order
+    begin
+    @order = Order.find(session[:order_id])
+  rescue ActiveRecord::RecordNotFound
+    @order = Order.create(status: "unsubmitted")
+    session[:order_id] = @order.id
+    end
+  end
+
+
     def set_order_item
       @order_item = OrderItem.find(params[:id])
     end
