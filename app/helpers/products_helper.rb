a048dfa9662c70d2f6b453e392e78fe2aca236de
Sylvia <sylviakieha@gmail.com>
2015-09-24 12:45:48 +0300

    displayed stock on order page and products index

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,11 +3,13 @@ module ProductsHelper
   number_to_currency price
   end
 
-  def print_stock(stock)
-    if stock > 0
+  def print_stock(stock, requested = 1)
+    if stock <= 0
+      content_tag(:span, "Out of Stock", class: "out_stock")
+    elsif stock >= requested
       content_tag(:span, "In Stock", class: "in_stock")
     else
-      content_tag(:span, "Out of Stock", class: "out_stock")
+      content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
     end
   end
 
