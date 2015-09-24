3ee81305c1b227a02c1d0afb958fd15289295f83
Sylvia <sylviakieha@gmail.com>
2015-09-24 23:18:53 +0300

    created a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_by_auth(auth_data)
+    user = where(provider: auth_data[:provider], uid: auth_data[:uid]).first_or_create
+    user.update(name:auth_data[:info][:name])
+  end
 end
