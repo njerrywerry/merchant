21e415e4b621246e944edc6df763bde754bb45a3
Sylvia <sylviakieha@gmail.com>
2015-09-25 00:07:53 +0300

    configured UI for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = @user
     redirect_to products_path, notice: "logged in"
  end
+
+ def destroy
+   session[:user_id] = nil
+   redirect_to root_path, notice: "Goodbye!"
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   match '/auth/:provider/callback', to: 'sessions#create', via: :get
 
+  match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/logout" => "sessions#destroy", as: :logout, via: :get
+
 end
