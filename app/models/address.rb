be6bd0ba0a30a90d5ef95e492ef80085f2f2e593
Sylvia <sylviakieha@gmail.com>
2015-09-26 23:56:18 +0300

    displayed addresses on the order page

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,9 @@ class Address < ActiveRecord::Base
   validates :county, presence: :true
   validates :postalcode, presence: :true, length: { is: 5 }, numericality: { only_integer: true }
   validates :city, presence: :true
+
+  def to_s
+    a = [line1, city, county, postalcode].compact.join(", ")
+  end
+
 end
